7b16d24d38053ae873cdeec7f90819d7bc1d02ad
Georg <gschra2@stanford.edu>
2022-10-20 10:37:26 -0700

    clean up ring example

--- examples/02_pet_recon/00_pet_projections.py
+++ examples/02_pet_recon/00_pet_projections.py
@@ -1,3 +1,4 @@
+import timeit
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -9,7 +10,7 @@ radius = 330
 num_sides = 28
 num_lor_endpoints_per_side = 16
 lor_spacing = 4.
-num_rings = 27
+num_rings = 45
 
 max_ring_difference = num_rings - 1
 
@@ -34,34 +35,44 @@ cd = ppd.RegularPolygonPETCoincidenceDescriptor(
 
 #----------------------------------------------------------------------------
 # get the start / end module and index number for the first 3 lors
-all_lors = np.arange(cd.num_lors)
-# choose 200 random lors
-lors = np.random.choice(all_lors, 200)
-
+lors = np.arange(cd.num_lors)
 start_mod, start_ind, end_mod, end_ind = cd.get_lor_indices(lors)
 xstart = scanner.get_lor_endpoints(start_mod, start_ind).astype(np.float32)
 xend = scanner.get_lor_endpoints(end_mod, end_ind).astype(np.float32)
 
 
+# setup a box like test image
+voxsize = np.array([2., 2., 2.], dtype=np.float32)
+n0 = int((ring_positions.max() - ring_positions.min()) / voxsize[0])
+n1 = 200
+n2 = 200
+
+# setup an image containing a square
+img = np.zeros((n0, n1, n2), dtype=np.float32)
+img[(n0 // 4):(3 * n0 // 4),
+    (n1 // 4):(3 * n1 // 4),
+    (n2 // 4):(3 * n2 // 4)] = 1
+
+# setup the image origin = the coordinate of the [0,0,0] voxel
+img_origin = (-(np.array(img.shape, dtype=np.float32) / 2) + 0.5) * voxsize
+img_fwd = np.zeros(cd.sinogram_spatial_shape, dtype=np.float32)
+
+ppw.joseph3d_fwd(xstart, xend, img, img_origin, voxsize, img_fwd)
+print(cd.sinogram_spatial_axis_order.name)
+#print(timeit.timeit('ppw.joseph3d_fwd(xstart, xend, img, img_origin, voxsize, img_fwd)', globals=globals()), number = 1)
+
 #--------------------------------------------------------------------------
 
-fig = plt.figure(figsize=(14, 7))
-ax1 = fig.add_subplot(1, 2, 1, projection='3d')
-ax2 = fig.add_subplot(1, 2, 2, projection='3d')
-scanner.show_lor_endpoints(ax1,
-                           show_linear_index=False,
-                           annotation_fontsize=0,
-                           s=1)
-scanner.show_lor_endpoints(ax2,
+fig = plt.figure(figsize=(7, 7))
+ax = fig.add_subplot(1, 1, 1, projection='3d')
+scanner.show_lor_endpoints(ax,
                            show_linear_index=False,
                            annotation_fontsize=0,
                            s=1)
 
-# show 200 random LORs
-cd.show_lors(lors, ax1)
-
-# show all LORs of a views in a single plane
-cd.show_view(ax2, cd.num_views // 2, scanner.num_rings // 2)
+# show two "views" in a single plane
+cd.show_view(ax, 0, 0)
+cd.show_view(ax, cd.num_views // 2, num_rings - 1, color='r')
 
 fig.tight_layout()
 fig.show()
