5fd904ae3d78d2cd8606841c2b194f0bd9233f93
Georg Schramm <gschra2@stanford.edu>
2022-11-05 20:09:11 -0700

    add first implementation of high level non-tof projector class

---
+++ examples/03_recon/00_pet_projections.py
@@ -0,0 +1,65 @@
+import numpy as np
+
+import pyparallelproj.scanners as scanners
+import pyparallelproj.coincidences as coincidences
+import pyparallelproj.subsets as subsets
+import pyparallelproj.petprojectors as petprojectors
+
+try:
+    import cupy as cp
+except:
+    import warnings
+    warnings.warn('cupy module not available')
+    import numpy as cp
+
+xp = np
+
+radius = 350
+num_sides = 28
+num_lor_endpoints_per_side = 16
+lor_spacing = 4.
+num_rings = 5
+
+max_ring_difference = num_rings - 1
+radial_trim = 49
+
+ring_positions = 5.55 * (np.arange(num_rings) - num_rings / 2 + 0.5)
+
+num_subsets = 28
+
+voxsize = (2., 2., 2.)
+num_trans = 300
+
+sinogram_order = 'PVR'
+symmetry_axis = 2
+
+img_shape = (123, 45, 5)
+img_origin = (0., 0., 0.)
+img = np.ones(img_shape)
+#---------------------------------------------------------------------
+scanner = scanners.RegularPolygonPETScannerGeometry(
+    radius,
+    num_sides,
+    num_lor_endpoints_per_side,
+    lor_spacing,
+    num_rings,
+    ring_positions,
+    symmetry_axis=symmetry_axis,
+    xp=xp)
+
+# setup the coincidence descriptor
+coincidence_descriptor = coincidences.RegularPolygonPETCoincidenceDescriptor(
+    scanner,
+    radial_trim=radial_trim,
+    max_ring_difference=max_ring_difference,
+    sinogram_spatial_axis_order=coincidences.
+    SinogramSpatialAxisOrder[sinogram_order])
+
+subsetter = subsets.SingoramViewSubsetter(coincidence_descriptor, num_subsets)
+
+projector = petprojectors.NonTOFPETJosephProjector(coincidence_descriptor,
+                                                   img_shape, img_origin,
+                                                   voxsize, subsetter)
+
+img_fwd = projector.forward(img)
+img_fwd_back = projector.adjoint(img_fwd)
---
+++ pyparallelproj/petprojectors.py
@@ -0,0 +1,120 @@
+import numpy.typing as npt
+
+import pyparallelproj.coincidences as coincidences
+import pyparallelproj.wrapper as ppw
+import pyparallelproj.subsets as subsets
+
+try:
+    import cupy.typing as cpt
+except:
+    import warnings
+    warnings.warn('cupy module not available')
+    import numpy.typing as cpt
+
+
+class NonTOFPETJosephProjector:
+
+    def __init__(self,
+                 coincidence_descriptor: coincidences.PETCoincidenceDescriptor,
+                 image_shape: tuple[int, int, int],
+                 image_origin: tuple[float, float,
+                                     float], voxel_size: tuple[float, float,
+                                                               float],
+                 subsetter: subsets.LORSubsetter) -> None:
+
+        self._coincidence_descriptor = coincidence_descriptor
+        self._image_shape = image_shape
+        self._image_origin = image_origin
+        self._voxel_size = voxel_size
+        self._subsetter = subsetter
+
+    @property
+    def coincidence_descriptor(self) -> coincidences.PETCoincidenceDescriptor:
+        return self._coincidence_descriptor
+
+    @property
+    def image_shape(self) -> tuple[int, int, int]:
+        return self._image_shape
+
+    @property
+    def image_origin(self) -> tuple[float, float, float]:
+        return self._image_origin
+
+    @property
+    def voxel_size(self) -> tuple[float, float, float]:
+        return self._voxel_size
+
+    @property
+    def subsetter(self) -> subsets.LORSubsetter:
+        return self._subsetter
+
+    @property
+    def xp(self):
+        return self.coincidence_descriptor.scanner.xp
+
+    def forward_subset(
+            self,
+            x: npt.NDArray | cpt.NDArray,
+            subset: int = 0,
+            lors: None | npt.NDArray = None) -> npt.NDArray | cpt.NDArray:
+
+        if lors is None:
+            lors = self.subsetter.get_subset_indices(subset)
+
+        start_mod, start_ind, end_mod, end_ind = self.coincidence_descriptor.get_lor_indices(
+            lors)
+        xstart = self.coincidence_descriptor.scanner.get_lor_endpoints(
+            start_mod, start_ind).astype(self.xp.float32)
+        xend = self.coincidence_descriptor.scanner.get_lor_endpoints(
+            end_mod, end_ind).astype(self.xp.float32)
+
+        image_forward = self.xp.zeros(xstart.shape[0], dtype=self.xp.float32)
+        #ppw.joseph3d_fwd(xstart, xend, x, self.image_origin, self.voxel_size,
+        #                 image_forward)
+
+        return image_forward
+
+    def forward(self,
+                x: npt.NDArray | cpt.NDArray) -> npt.NDArray | cpt.NDArray:
+
+        image_forward = self.xp.zeros(self.coincidence_descriptor.num_lors,
+                                      dtype=self.xp.float32)
+
+        for subset in range(self.subsetter.num_subsets):
+            lors = self.subsetter.get_subset_indices(subset)
+            image_forward[lors] = self.forward_subset(x, lors=lors)
+
+        return image_forward
+
+    def adjoint_subset(
+            self,
+            y_subset: npt.NDArray | cpt.NDArray,
+            subset: int = 0,
+            lors: None | npt.NDArray = None) -> npt.NDArray | cpt.NDArray:
+
+        if lors is None:
+            lors = self.subsetter.get_subset_indices(subset)
+
+        start_mod, start_ind, end_mod, end_ind = self.coincidence_descriptor.get_lor_indices(
+            lors)
+        xstart = self.coincidence_descriptor.scanner.get_lor_endpoints(
+            start_mod, start_ind).astype(self.xp.float32)
+        xend = self.coincidence_descriptor.scanner.get_lor_endpoints(
+            end_mod, end_ind).astype(self.xp.float32)
+
+        back_image = self.xp.zeros(self.image_shape, dtype=self.xp.float32)
+        #ppw.joseph3d_back(xstart, xend, back_image, self.image_origin,
+        #                  self.voxel_size, y_subset)
+
+        return back_image
+
+    def adjoint(self,
+                y: npt.NDArray | cpt.NDArray) -> npt.NDArray | cpt.NDArray:
+
+        back_image = self.xp.zeros(self.image_shape, dtype=self.xp.float32)
+
+        for subset in range(self.subsetter.num_subsets):
+            lors = self.subsetter.get_subset_indices(subset)
+            back_image += self.adjoint_subset(y[lors])
+
+        return back_image
