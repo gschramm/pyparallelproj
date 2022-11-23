360b946037001e4336d6bc74ad584df6d5d45aa3
Georg <gschra2@stanford.edu>
2022-11-22 16:30:51 -0800

    refine pdhg example

--- examples/03_recon/03_pdhg.py
+++ examples/03_recon/03_pdhg.py
@@ -13,10 +13,12 @@ except:
     warnings.warn('cupy module not available')
     import numpy as cp
 
-beta = 1e-1
-noise_level = 0.2
+beta = 1e0
 xp = np
 n = 20
+num_iter = 5000
+
+noise = 'poisson'
 
 #--------------------------------------------------------------------------
 
@@ -25,26 +27,34 @@ xp.random.seed(4)
 x_true = xp.pad(xp.random.rand(n - 4), 2)
 
 data_operator = operators.MatrixOperator(
-    xp.random.rand(2 * x_true.shape[0], x_true.shape[0]), xp)
+    5 * xp.random.rand(2 * x_true.shape[0], x_true.shape[0]), xp)
 prior_operator = operators.GradientOperator(data_operator.input_shape, xp)
 prior_norm = functionals.L2L1Norm(xp)
 
-data = data_operator.forward(x_true) + noise_level * xp.random.randn(
-    *data_operator.output_shape)
+x_fwd = data_operator.forward(x_true)
 
-data_distance = functionals.SquaredL2NormDistance(data, xp)
+if noise == 'gaussian':
+    data = x_fwd + 0.1 * x_fwd.mean() * xp.random.randn(
+        *data_operator.output_shape)
+    data_distance = functionals.SquaredL2NormDistance(data, xp)
+elif noise == 'poisson':
+    data = xp.random.poisson(x_fwd)
+    data_distance = functionals.NegativePoissonLogLikelihood(data, xp)
+else:
+    raise ValueError
 
-sigma = 0.5 / float(data_operator.norm())
-tau = 0.5 / float(data_operator.norm())
+sigma = 0.9 / float(data_operator.norm())
+tau = 0.9 / float(data_operator.norm())
 
 pdhg = algorithms.PDHG(data_operator, data_distance, prior_operator,
                        prior_norm, beta, sigma, tau)
 
-pdhg.run(1000, calculate_cost=True)
+pdhg.run(num_iter, calculate_cost=True)
 
 cost_func = lambda x: data_distance(data_operator.forward(
     x)) + beta * prior_norm(prior_operator.forward(x))
 
+# use powell optimizer as reference
 from scipy.optimize import fmin_powell
 
 res = fmin_powell(cost_func, pdhg.x)
