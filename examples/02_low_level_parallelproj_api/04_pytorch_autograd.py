39c17bbe36789d73928b46bc55706bbbdb6d9c9e
Georg <gschra2@stanford.edu>
2023-01-17 14:58:23 -0800

    add more advanced auto grad example

--- examples/02_low_level_parallelproj_api/04_pytorch_autograd.py
+++ examples/02_low_level_parallelproj_api/04_pytorch_autograd.py
@@ -1,53 +1,105 @@
-""" basic example on how to use cupy and pytorch autograd """
-import cupy
+"""https://pytorch.org/tutorials/beginner/examples_autograd/two_layer_net_custom_function.html
+   https://docs.cupy.dev/en/stable/user_guide/interoperability.html#using-custom-kernels-in-pytorch
+"""
+
 import torch
+import cupy
+import math
 
 
-class CuPyLog(torch.autograd.Function):
-    """ simple autograd function that calculates element wise log(x) using cupy """
+class LegendrePolynomial3(torch.autograd.Function):
+    """
+    We can implement our own custom autograd Functions by subclassing
+    torch.autograd.Function and implementing the forward and backward passes
+    which operate on Tensors.
+    """
 
     @staticmethod
     def forward(ctx, x):
-        ctx.input = x
-        # Enforce contiguous arrays to simplify RawKernel indexing.
+        """
+        In the forward pass we receive a Tensor containing the input and return
+        a Tensor containing the output. ctx is a context object that can be used
+        to stash information for backward computation. You can cache arbitrary
+        objects for use in the backward pass using the ctx.save_for_backward method.
+        """
+        ctx.save_for_backward(x)
         cupy_x = cupy.ascontiguousarray(cupy.from_dlpack(x.detach()))
 
-        #-------------------------------------------------------------
-        #--- the actual function -------------------------------------
-        #-------------------------------------------------------------
-        cupy_y = cupy.log(cupy_x)
-        #-------------------------------------------------------------
-        #-------------------------------------------------------------
-        #-------------------------------------------------------------
+        # a custom function written in cupy
+        cupy_y = 0.5 * (5 * cupy_x**3 - 3 * cupy_x)
 
-        # the ownership of the device memory backing cupy_y is implicitly
-        # transferred to torch_y, so this operation is safe even after
-        # going out of scope of this function.
         torch_y = torch.from_dlpack(cupy_y)
+
         return torch_y
 
     @staticmethod
-    def backward(ctx, grad_y):
-        # Enforce contiguous arrays to simplify RawKernel indexing.
-        cupy_input = cupy.from_dlpack(ctx.input.detach()).ravel()
-        cupy_grad_y = cupy.from_dlpack(grad_y.detach()).ravel()
+    def backward(ctx, grad_output):
+        """
+        In the backward pass we receive a Tensor containing the gradient of the loss
+        with respect to the output, and we need to compute the gradient of the loss
+        with respect to the input.
+        """
+        x, = ctx.saved_tensors
+        cupy_x = cupy.from_dlpack(x.detach()).ravel()
 
-        #-------------------------------------------------------------
-        #---- gradient of the function -------------------------------
-        #-------------------------------------------------------------
-        cupy_grad_x = cupy_grad_y / cupy_input
-        #-------------------------------------------------------------
-        #-------------------------------------------------------------
-        #-------------------------------------------------------------
+        # gradient of the custom function written in cupy
+        cupy_grad_x = 0.5 * (15 * cupy_x**2 - 3)
 
-        # the ownership of the device memory backing cupy_grad_x is implicitly
-        # transferred to torch_y, so this operation is safe even after
-        # going out of scope of this function.
-        torch_grad_x = torch.from_dlpack(cupy_grad_x).reshape(ctx.input.shape)
+        torch_grad_x = grad_output * torch.from_dlpack(cupy_grad_x)
 
         return torch_grad_x
 
 
-if __name__ == '__main__':
-    x = torch.rand(3, 4, 5, requires_grad=True, device='cuda')
-    torch.autograd.gradcheck(CuPyLog.apply, x, eps=1e-4)
+dtype = torch.float64
+device = torch.device("cuda:0")  # Uncomment this to run on GPU
+
+# Create Tensors to hold input and outputs.
+# By default, requires_grad=False, which indicates that we do not need to
+# compute gradients with respect to these Tensors during the backward pass.
+x = torch.linspace(-math.pi, math.pi, 200, device=device, dtype=dtype)
+y = torch.sin(x)
+
+# Create random Tensors for weights. For this example, we need
+# 4 weights: y = a + b * P3(c + d * x), these weights need to be initialized
+# not too far from the correct result to ensure convergence.
+# Setting requires_grad=True indicates that we want to compute gradients with
+# respect to these Tensors during the backward pass.
+a = torch.full((), 0.5, device=device, dtype=dtype, requires_grad=True)
+b = torch.full((), -1.0, device=device, dtype=dtype, requires_grad=True)
+c = torch.full((), 0.1, device=device, dtype=dtype, requires_grad=True)
+d = torch.full((), 0.3, device=device, dtype=dtype, requires_grad=True)
+
+# To apply our Function, we use Function.apply method. We alias this as 'P3'.
+P3 = LegendrePolynomial3.apply
+# check the gradients using gradcheck
+torch.autograd.gradcheck(P3, c + d * x, eps=1e-6)
+
+learning_rate = 5e-5
+for t in range(2000):
+
+    # Forward pass: compute predicted y using operations; we compute
+    # P3 using our custom autograd operation.
+    y_pred = a + b * P3(c + d * x)
+
+    # Compute and print loss
+    loss = (y_pred - y).pow(2).sum()
+    if t % 100 == 99:
+        print(t, loss.item())
+
+    # Use autograd to compute the backward pass.
+    loss.backward()
+
+    # Update weights using gradient descent
+    with torch.no_grad():
+        a -= learning_rate * a.grad
+        b -= learning_rate * b.grad
+        c -= learning_rate * c.grad
+        d -= learning_rate * d.grad
+
+        # Manually zero the gradients after updating weights
+        a.grad = None
+        b.grad = None
+        c.grad = None
+        d.grad = None
+
+print(f'Result: y = {a.item()} + {b.item()} * P3({c.item()} + {d.item()} x)')
