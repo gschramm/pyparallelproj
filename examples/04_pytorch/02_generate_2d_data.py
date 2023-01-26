193ba3d7c5e460aad91a5b2e9e53976ee25a82b0
Georg <gschra2@stanford.edu>
2023-01-25 18:00:58 -0800

    add script to simulate 2D OSEM data from brainweb

---
+++ examples/04_pytorch/02_generate_2d_data.py
@@ -0,0 +1,221 @@
+"""script to generate 2D PET data based on the brainweb phantom"""
+
+#-----------------------------------------------------------------------
+#-----------------------------------------------------------------------
+#
+# make sure to run the script "download_brainweb_petmr.py" in ../data
+# before running this script
+#
+#-----------------------------------------------------------------------
+#-----------------------------------------------------------------------
+
+import json
+from pathlib import Path
+
+import numpy as np
+import nibabel as nib
+import dill
+
+import pyparallelproj.coincidences as coincidences
+import pyparallelproj.subsets as subsets
+import pyparallelproj.tof as tof
+import pyparallelproj.petprojectors as petprojectors
+import pyparallelproj.resolution_models as resolution_models
+import pyparallelproj.algorithms as algorithms
+
+import cupy as cp
+import cupyx.scipy.ndimage as ndi
+
+#---------------------------------------------------------------------
+#---------------------------------------------------------------------
+#--- input parmeters -------------------------------------------------
+#---------------------------------------------------------------------
+#---------------------------------------------------------------------
+
+# brainweb subset number
+# [4, 5, 6, 18, 20, 38, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54]
+subject_number = 38
+# simulation number [0,1,2]
+sim_number = 0
+# slice number 0 ... 128
+start_sl = 64
+
+#-------------------
+# reconstruction parameters
+num_iterations = 4
+num_subsets = 34
+
+#-------------------
+# scanner parameters
+num_rings = 1
+symmetry_axis = 0
+fwhm_mm_data = 4.5
+fwhm_mm_recon = 4.5
+
+#-------------------
+# sinogram (data order) parameters
+sinogram_order = 'RVP'
+
+#-------------------
+# image parameters
+voxel_size = (2., 2., 2.)
+
+# number of true emitted coincidences per volume (mm^3)
+# 5 -> low counts, 5 -> medium counts, 500 -> high counts
+trues_per_volume = 50.
+
+seed = 1
+
+cp.random.seed(seed)
+
+#---------------------------------------------------------------------
+#---------------------------------------------------------------------
+#--- setup the emission and attenuation images -----------------------
+#---------------------------------------------------------------------
+#---------------------------------------------------------------------
+
+nii = nib.as_closest_canonical(
+    nib.load(
+        f'../data/brainweb_petmr/subject{subject_number:02}/sim_{sim_number}/true_pet.nii.gz'
+    ))
+image = cp.array(nii.get_fdata(), dtype=cp.float32)
+
+# downsample image by a factor of 2, to get 2mm voxels
+image = (image[::2, :, :] + image[1::2, :, :]) / 2
+image = (image[:, ::2, :] + image[:, 1::2, :]) / 2
+image = (image[:, :, ::2] + image[:, :, 1::2]) / 2
+
+num_axial = 1
+
+image = image[:, :, start_sl:(start_sl + 1)]
+
+image_shape = image.shape
+
+image_origin = tuple(
+    (-0.5 * image_shape[i] + 0.5) * voxel_size[i] for i in range(3))
+
+attenuation_image = (0.01 * (image > 0)).astype(cp.float32)
+
+#---------------------------------------------------------------------
+#--- setup of the PET forward model (the projector) ------------------
+#---------------------------------------------------------------------
+
+coincidence_descriptor = coincidences.GEDiscoveryMICoincidenceDescriptor(
+    num_rings=num_rings,
+    sinogram_spatial_axis_order=coincidences.
+    SinogramSpatialAxisOrder[sinogram_order],
+    radial_trim=171,
+    xp=cp)
+
+#---------------------------------------------------------------------
+# setup a non-time-of-flight and time-of-flight projector
+# to calculate the attenuation factors based on the attenuation image
+projector = petprojectors.PETJosephProjector(coincidence_descriptor,
+                                             image_shape, image_origin,
+                                             voxel_size)
+
+# simulate the attenuation factors (exp(-fwd(attenuation_image)))
+attenuation_factors = cp.exp(-projector.forward(attenuation_image))
+
+# use GE DMI TOF parameters, but only 19 since we are only reconstructing
+# the center of the FOV (brain scan)
+tof_parameters = tof.ge_discovery_mi_tof_parameters
+tof_parameters.num_tofbins = 19
+projector.tof_parameters = tof_parameters
+
+#--------------------------------------------------------------------------
+# use an image-based resolution model in the projector to model the effect
+# of limited resolution
+res_model = resolution_models.GaussianImageBasedResolutionModel(
+    image_shape, tuple(fwhm_mm_data / (2.35 * x) for x in voxel_size), cp, ndi)
+
+projector.image_based_resolution_model = res_model
+
+#--------------------------------------------------------------------------
+# set the multiplicative corrections in the scanner to model photon attenuation
+# and scanner sensitivity
+
+projector.multiplicative_corrections = attenuation_factors
+
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+#--- simulate acquired data based on forward model and known contaminations ---
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+
+image_fwd = projector.forward(image)
+
+# scale the image such that we get a certain true count per emission voxel value
+emission_volume = cp.where(image > 0)[0].shape[0] * np.prod(voxel_size)
+current_trues_per_volume = float(image_fwd.sum() / emission_volume)
+
+scaling_factor = (trues_per_volume / current_trues_per_volume)
+image *= scaling_factor
+image_fwd *= scaling_factor
+
+# simulate a constant background contamination
+contamination = cp.full(projector.output_shape,
+                        image_fwd.mean(),
+                        dtype=cp.float32)
+
+# generate noisy data
+data = cp.random.poisson(image_fwd + contamination).astype(cp.uint16)
+
+#---------------------------------------------------------------------
+#---------------------------------------------------------------------
+#--- run OSEM reconstruction -----------------------------------------
+#---------------------------------------------------------------------
+#---------------------------------------------------------------------
+
+# add a subsetter to the projector such that we can do updates with ordered subsets
+subsetter = subsets.SingoramViewSubsetter(coincidence_descriptor, num_subsets)
+projector.subsetter = subsetter
+
+# update the resolution model which can be different for the reconstruction
+res_model_recon = resolution_models.GaussianImageBasedResolutionModel(
+    image_shape, tuple(fwhm_mm_recon / (2.35 * x) for x in voxel_size), cp,
+    ndi)
+
+projector.image_based_resolution_model = res_model_recon
+
+reconstructor = algorithms.OSEM(data, contamination, projector, verbose=True)
+reconstructor.run(num_iterations, evaluate_cost=False)
+
+x = reconstructor.x
+
+#---------------------------------------------------------------------
+#---------------------------------------------------------------------
+#--- save the data we need later -------------------------------------
+#---------------------------------------------------------------------
+#---------------------------------------------------------------------
+
+odir = Path(
+    '..'
+) / 'data' / f'OSEM_2D_{trues_per_volume:.2E}' / f'{subject_number:03}_{sim_number:03}_{start_sl:03}'
+odir.mkdir(parents=True, exist_ok=True)
+
+# save the true image
+cp.save(odir / 'image.npy', image)
+cp.save(odir / f'osem_{seed:03}.npy', x)
+cp.save(odir / f'data_{seed:03}.npy', data)
+cp.save(odir / 'image_fwd.npy', image_fwd)
+cp.save(odir / 'multiplicative_corrections.npy',
+        projector.multiplicative_corrections)
+cp.save(odir / 'contamination.npy', contamination)
+
+# pickle (dill) the projector, but without multiplicative corrections
+projector.multiplicative_corrections = None
+
+with open(odir / 'projector.pkl', 'wb') as f:
+    dill.dump(projector, f)
+
+with open(odir / 'parameters.json', 'w') as f:
+    json.dump(
+        {
+            i: eval(i)
+            for i in [
+                "num_iterations", "num_subsets", "subject_number",
+                "sim_number", "start_sl", "voxel_size", "trues_per_volume",
+                "seed", "fwhm_mm_data", "fwhm_mm_recon"
+            ]
+        }, f)
