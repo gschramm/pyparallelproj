dfd1dd88ab6a623afbe06ed81228088a6cc0246b
Georg <gschra2@stanford.edu>
2023-01-26 11:36:56 -0800

    add normalization to data loader

--- examples/04_pytorch/04_datasets.py
+++ examples/04_pytorch/04_datasets.py
@@ -1,5 +1,3 @@
-# TODO data normalization
-
 import torch
 import numpy as np
 from pathlib import Path
@@ -9,9 +7,22 @@ class OSEM2DDataSet(torch.utils.data.Dataset):
 
     def __init__(self,
                  basedir: str = '../data/OSEM_2D_5.00E+01',
-                 seed: int = 1):
+                 seed: int = 1,
+                 normalization_quantile: float = 0.99) -> None:
+        """simulated 2D PET data
+
+        Parameters
+        ----------
+        basedir : str, optional
+            base directory containing all simulated 2D data sets, by default '../data/OSEM_2D_5.00E+01'
+        seed : int, optional
+            seed used for random generator, by default 1
+        normalization_quantile : float, optional
+            quantile used to normalize the OSEM and true image, by default 0.99
+        """
         self._basedir = Path(basedir)
         self._seed = seed
+        self._normalization_quantile = normalization_quantile
 
         self._dir_list = sorted(self._basedir.glob('???_???_???'))
 
@@ -31,10 +42,31 @@ class OSEM2DDataSet(torch.utils.data.Dataset):
     def dir_list(self) -> list[Path]:
         return self._dir_list
 
-    def __len__(self):
+    @property
+    def normalization_quantile(self) -> float:
+        return self._normalization_quantile
+
+    def __len__(self) -> int:
         return len(self.dir_list)
 
-    def __getitem__(self, idx: int):
+    def __getitem__(
+        self, idx: int
+    ) -> tuple[tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor,
+                     torch.Tensor], torch.Tensor]:
+        """load sample of OSEM 2D data set
+
+        Parameters
+        ----------
+        idx : int
+            sample number
+
+        Returns
+        -------
+        tuple[tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor], torch.Tensor]
+          (normalized OSEM recon, noisy emission data (sinogram), multiplicative_corrections sinogram, 
+           contamination sinogram, image normalization factor, 
+           normalized true image
+        """
         odir = self.dir_list[idx]
 
         image = torch.from_numpy(np.expand_dims(np.load(odir / 'image.npy'),
@@ -47,7 +79,14 @@ class OSEM2DDataSet(torch.utils.data.Dataset):
             np.load(odir / 'multiplicative_corrections.npy'))
         contamination = torch.from_numpy(np.load(odir / 'contamination.npy'))
 
-        return (osem, data, multiplicative_corrections, contamination), image
+        # normalize the OSEM and true image
+        norm = torch.quantile(osem, self.normalization_quantile)
+
+        osem /= norm
+        image /= norm
+
+        return (osem, data, multiplicative_corrections, contamination,
+                norm), image
 
 
 if __name__ == '__main__':
