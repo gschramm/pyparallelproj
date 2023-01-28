e01ba56c3d87cebdb3a5eb2bda227c8c9a336ff3
Georg <gschra2@stanford.edu>
2023-01-27 17:35:38 -0800

    polish varnet training script

--- examples/04_pytorch/03_supervised_varnet.py
+++ examples/04_pytorch/03_supervised_varnet.py
@@ -9,6 +9,7 @@ import dill
 
 import pyparallelproj.petprojectors as petprojectors
 
+from pathlib import Path
 from layers import LinearSubsetForwardLayer, LinearSubsetAdjointLayer
 from datasets import OSEM2DDataSet
 from models import sequential_conv_model
@@ -135,61 +136,62 @@ class PETVarNet(torch.nn.Module):
         return x
 
 
-if __name__ == '__main__':
-    dtype = torch.float32
-    device = torch.device("cuda:0")
-    training_data_dir: str = '../data/training/OSEM_2D_5.00E+01'
-    batch_size: int = 16
-    learning_rate: float = 1e-3
-    num_epochs: int = 100
-    loss_fct = torch.nn.L1Loss()
-    num_blocks: int = 4
-    num_layers: int = 6
-    num_features: int = 10
+#---------------------------------------------------------------------
 
-    #---------------------------------------------------------------------------
-    #--- setup the data loaders ------------------------------------------------
-    #---------------------------------------------------------------------------
 
-    training_data_set = OSEM2DDataSet(training_data_dir)
-    training_data_loader = torch.utils.data.DataLoader(training_data_set,
-                                                       batch_size=batch_size,
-                                                       drop_last=True,
-                                                       shuffle=True,
-                                                       num_workers=5)
-
-    #---------------------------------------------------------------------------
-    #--- load the projector ----------------------------------------------------
-    #---------------------------------------------------------------------------
-
-    # the projector (without multiplicative corrections) should be the same for
-    # all data sets, so we only restore the one from the first data set
-    with open(training_data_set.dir_list[0] / 'projector.pkl', 'rb') as f:
-        projector = dill.load(f)
-
-    #---------------------------------------------------------------------------
-    #--- define a simple conv net that maps an image onto an image -------------
-    #---------------------------------------------------------------------------
-    conv_net = sequential_conv_model(device=torch.device("cuda:0"),
-                                     kernel_size=(3, 3, 1),
-                                     num_layers=num_layers,
-                                     num_features=num_features,
-                                     dtype=torch.float32)
-
-    model = PETVarNet(projector, conv_net, num_blocks=num_blocks)
-
-    # setup the optimizer
-    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-
-    #---------------------------------------------------------------------------
-    #--- training loop ---------------------------------------------------------
-    #---------------------------------------------------------------------------
+def training_loop(dataloader, model, loss_fn, optimizer):
 
     loss_list = []
 
-    for epoch in range(num_epochs):
+    for i, (osem, data, multiplicative_corrections, contamination,
+            adjoint_ones, norm, image) in enumerate(dataloader):
+
+        # send mini-batch of data to cuda device
+        # normalized OSEM recon - shape (batch_size, 1, n0, n1, 1)
+        osem = osem.to(device)
+        # emission sinograms - shape (batch_size, num_subsets, num_subset_lors, num_tof_bins)
+        data = data.to(device)
+        # mult. correction sinograms - shape (batch_size, num_subsets, num_subset_lors, 1)
+        multiplicative_corrections = multiplicative_corrections.to(device)
+        # contamination sinograms - shape (batch_size, num_subsets, num_subset_lors, num_tof_bins)
+        contamination = contamination.to(device)
+        # adjoint ones (sens. images) - shape (batch_size, num_subsets, n0, n1, 1)
+        adjoint_ones = adjoint_ones.to(device)
+        # normalization factor used to normalize OSEM image - shape (batch_size,)
+        norm = norm.to(device)
+        # normalized ground truth images (batch_size, 1, n0, n1, 1)
+        image = image.to(device)
+
+        x_fwd = model.forward(osem, data, multiplicative_corrections,
+                              contamination, adjoint_ones, norm)
+
+        loss = loss_fn(x_fwd, image)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        loss_list.append(float(loss.cpu().detach().numpy()))
+
+        if i % 30 == 0:
+            print(
+                f'{(i+1):03} / {(dataloader.dataset.__len__() // batch_size):03} loss: {loss_list[-1]:.2E}'
+            )
+
+    return loss_list
+
+
+#------------------------------------------------------------------------------------------------------
+
+
+def validation_loop(dataloader, model, loss_fn, save_dir):
+
+    num_batches = len(dataloader)
+    val_loss = 0
+
+    with torch.no_grad():
         for i, (osem, data, multiplicative_corrections, contamination,
-                adjoint_ones, norm, image) in enumerate(training_data_loader):
+                adjoint_ones, norm, image) in enumerate(dataloader):
 
             # send mini-batch of data to cuda device
             # normalized OSEM recon - shape (batch_size, 1, n0, n1, 1)
@@ -210,49 +212,207 @@ if __name__ == '__main__':
             x_fwd = model.forward(osem, data, multiplicative_corrections,
                                   contamination, adjoint_ones, norm)
 
-            loss = loss_fct(x_fwd, image)
-            loss_list.append(float(loss.cpu().detach().numpy()))
+            val_loss += loss_fn(x_fwd, image)
 
-            print(
-                f'{epoch:03} / {(i+1):03} / {(ds.__len__() // batch_size):03} loss: {loss:.2E}'
-            )
+    val_loss /= num_batches
 
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
+    print(f'validation loss: {val_loss:.2E}')
 
-        # save some visualizations at the end of every epoch
-        y = conv_net(osem)
-        for ib in range(batch_size):
-            fig, ax = plt.subplots(2, 2, figsize=(9, 9))
-            ax[0, 0].imshow(osem[ib, 0, ...].cpu().detach().numpy().squeeze(),
-                            vmin=0,
-                            vmax=1.2)
-            ax[0, 1].imshow(x_fwd[ib, 0, ...].cpu().detach().numpy().squeeze(),
-                            vmin=0,
-                            vmax=1.2)
-            ax[1, 0].imshow(image[ib, 0, ...].cpu().detach().numpy().squeeze(),
-                            vmin=0,
-                            vmax=1.2)
-            ax[1, 1].imshow(y[ib, 0, ...].cpu().detach().numpy().squeeze())
+    show_validation_batch(osem, x_fwd, image, model, save_dir)
 
-            for axx in ax.ravel():
-                axx.set_axis_off()
-            fig.tight_layout()
-            fig.savefig(f'sample_{ib:02}.png')
-            plt.close()
+    return val_loss
 
-            if len(loss_list) > 505:
-                loss_array = np.array(loss_list)
-                fig2, ax2 = plt.subplots(2, 2, figsize=(8, 8))
-                ax2[0, 0].loglog(
-                    np.convolve(loss_array, np.ones(5) / 5, mode='valid'))
-                ax2[0, 1].loglog(
-                    np.convolve(loss_array, np.ones(25) / 25, mode='valid'))
-                ax2[1, 0].loglog(
-                    np.convolve(loss_array, np.ones(100) / 100, mode='valid'))
-                ax2[1, 1].loglog(
-                    np.convolve(loss_array, np.ones(500) / 500, mode='valid'))
-                fig2.tight_layout()
-                fig2.savefig(f'loss.png')
-                plt.close()
+
+def show_validation_batch(input, output, gt, mdl, save_dir):
+
+    conv_output = mdl._neural_net(input).cpu().detach().numpy().squeeze()
+
+    for ib in range(input.shape[0]):
+        fig, ax = plt.subplots(2, 2, figsize=(9, 9))
+        ax[0, 0].imshow(input[ib, 0, ...].cpu().detach().numpy().squeeze(),
+                        vmin=0,
+                        vmax=1.2)
+        ax[0, 1].imshow(output[ib, 0, ...].cpu().detach().numpy().squeeze(),
+                        vmin=0,
+                        vmax=1.2)
+        ax[1, 0].imshow(gt[ib, 0, ...].cpu().detach().numpy().squeeze(),
+                        vmin=0,
+                        vmax=1.2)
+
+        vmax = np.abs(conv_output[ib, ...]).max()
+        ax[1, 1].imshow(conv_output[ib, ...],
+                        vmin=-vmax,
+                        vmax=vmax,
+                        cmap=plt.cm.seismic)
+
+        for axx in ax.ravel():
+            axx.set_axis_off()
+        fig.tight_layout()
+        fig.savefig(save_dir / f'sample_{ib:02}.png')
+        plt.close()
+
+
+def plot_training_loss(loss_list: list[float], save_dir: Path):
+    loss_array = np.array(loss_list)
+    fig2, ax2 = plt.subplots(2, 2, figsize=(8, 8))
+    if loss_array.size > 5:
+        sm_ker = np.ones(5) / 5
+        ax2[0, 0].loglog(np.convolve(loss_array, sm_ker, mode='valid'))
+    if loss_array.size > 25:
+        sm_ker = np.ones(25) / 25
+        ax2[0, 1].loglog(np.convolve(loss_array, sm_ker, mode='valid'))
+    if loss_array.size > 100:
+        sm_ker = np.ones(100) / 100
+        ax2[1, 0].loglog(np.convolve(loss_array, sm_ker, mode='valid'))
+    if loss_array.size > 500:
+        sm_ker = np.ones(500) / 500
+        ax2[1, 1].loglog(np.convolve(loss_array, sm_ker, mode='valid'))
+    fig2.tight_layout()
+    fig2.savefig(save_dir / f'training_loss.png')
+    plt.close()
+
+
+def plot_validation_loss(loss: np.ndarray, save_dir: Path):
+    inds = np.where(loss > 0)
+    fig3, ax3 = plt.subplots(1, 3, figsize=(9, 3))
+    ax3[0].plot(loss[inds])
+    if loss[inds].size > 3:
+        sm_ker = np.ones(3) / 3
+        ax3[1].plot(np.convolve(loss[inds], sm_ker, mode='valid'))
+    if loss[inds].size > 5:
+        sm_ker = np.ones(5) / 5
+        ax3[2].plot(np.convolve(loss[inds], sm_ker, mode='valid'))
+    fig3.tight_layout()
+    fig3.savefig(save_dir / f'validation_loss.png')
+    plt.close()
+
+
+#------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------
+
+if __name__ == '__main__':
+
+    import argparse
+    import json
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--num_epochs', type=int, default=10)
+    parser.add_argument('--batch_size', type=int, default=16)
+    parser.add_argument('--learning_rate', type=float, default=1e-3)
+    parser.add_argument('--num_blocks', type=int, default=4)
+    parser.add_argument('--num_layers', type=int, default=6)
+    parser.add_argument('--num_features', type=int, default=10)
+    args = parser.parse_args()
+
+    num_epochs: int = args.num_epochs
+    batch_size: int = args.batch_size
+    learning_rate: float = args.learning_rate
+    num_blocks: int = args.num_blocks
+    num_layers: int = args.num_layers
+    num_features: int = args.num_features
+
+    training_data_dir: str = '../data/training/OSEM_2D_5.00E+01'
+    validation_data_dir: str = '../data/validation/OSEM_2D_5.00E+01'
+
+    dtype = torch.float32
+    device = torch.device("cuda:0")
+    loss_fct = torch.nn.L1Loss()
+
+    i_out = 0
+    output_dir = Path(f'run/{i_out:04}')
+
+    while output_dir.exists():
+        i_out += 1
+        output_dir = Path(f'run/{i_out:04}')
+
+    output_dir.mkdir(exist_ok=True, parents=True)
+
+    # save input configuration
+    with open(output_dir / 'config.json', 'w') as f:
+        json.dump(vars(args), f)
+
+    #---------------------------------------------------------------------------
+    #--- setup the data loaders ------------------------------------------------
+    #---------------------------------------------------------------------------
+
+    training_data_set = OSEM2DDataSet(training_data_dir)
+    training_data_loader = torch.utils.data.DataLoader(
+        training_data_set,
+        batch_size=batch_size,
+        drop_last=True,
+        shuffle=True,
+        num_workers=5,
+        pin_memory=True,
+        pin_memory_device='cuda:0')
+
+    validation_data_set = OSEM2DDataSet(validation_data_dir)
+    validation_data_loader = torch.utils.data.DataLoader(
+        validation_data_set,
+        batch_size=batch_size,
+        drop_last=True,
+        shuffle=True,
+        num_workers=5,
+        pin_memory=True,
+        pin_memory_device='cuda:0')
+
+    #---------------------------------------------------------------------------
+    #--- load the projector ----------------------------------------------------
+    #---------------------------------------------------------------------------
+
+    # the projector (without multiplicative corrections) should be the same for
+    # all data sets, so we only restore the one from the first data set
+    with open(training_data_set.dir_list[0] / 'projector.pkl', 'rb') as f:
+        projector = dill.load(f)
+
+    #---------------------------------------------------------------------------
+    #--- define a simple conv net that maps an image onto an image -------------
+    #---------------------------------------------------------------------------
+    conv_net = sequential_conv_model(device=device,
+                                     kernel_size=(3, 3, 1),
+                                     num_layers=num_layers,
+                                     num_features=num_features,
+                                     dtype=torch.float32)
+
+    model = PETVarNet(projector, conv_net, num_blocks=num_blocks)
+
+    # setup the optimizer
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+    #---------------------------------------------------------------------------
+    #--- training loop ---------------------------------------------------------
+    #---------------------------------------------------------------------------
+
+    training_loss = []
+    validation_loss = np.zeros(num_epochs)
+
+    for epoch in range(num_epochs):
+        print(f'epoch {(epoch+1):04} / {num_epochs:04}')
+        training_loss += training_loop(training_data_loader, model, loss_fct,
+                                       optimizer)
+        validation_loss[epoch] = validation_loop(validation_data_loader, model,
+                                                 loss_fct, output_dir)
+
+        # save the last checkpoint
+        torch.save(
+            {
+                'epoch': epoch,
+                'model_state_dict': model.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),
+                'loss': training_loss[-1],
+            }, output_dir / 'last_model.cpt')
+
+        # if the validation loss in minimal so far, save to a separate file
+        if validation_loss[epoch] == validation_loss[
+                validation_loss > 0].min():
+            torch.save(
+                {
+                    'epoch': epoch,
+                    'model_state_dict': model.state_dict(),
+                    'optimizer_state_dict': optimizer.state_dict(),
+                    'loss': training_loss[-1],
+                }, output_dir / 'model_best_val_loss.cpt')
+
+        # plot the loss functions to files
+        plot_training_loss(training_loss, output_dir)
+        plot_validation_loss(validation_loss, output_dir)
